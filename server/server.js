dfbb95303eae94e789671259754e803fac2aafe8
yumimac <yumi.ko.cheers@gmail.com>
2016-07-22 13:58:28 +1000

    console log node listening to port

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-var debug = require('debug')('node-sequelize-postgresql');
+// var debug = require('debug')('node-sequelize-postgresql');
 var app = require('./app');
 var models = require('./models');
 
-app.set('port', process.env.PORT || 3001);
+app.set('port', (process.env.PORT || 3001));
 
 models.sequelize.sync().then(function () {
-    var server = app.listen(app.get('port'), function() {
-        debug('Express server listening on port ' + server.address().port);
-    });
+  var server = app.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + server.address().port);
+  });
 });
